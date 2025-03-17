e32a63523d3603e14891571e1730eefb49a4c7b4
Raphael Vullriede <raphael@osslabz.net>
2025-03-17 09:18:26 +0100

    feat: update bitcoinj version

--- src/test/java/net/osslabz/bitcoin/BitcoinUtilsTest.java
+++ src/test/java/net/osslabz/bitcoin/BitcoinUtilsTest.java
@@ -2,15 +2,17 @@ package net.osslabz.bitcoin;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.bitcoinj.base.BitcoinNetwork;
 import org.junit.jupiter.api.Test;
 
-public class BitcoinUtilsTest {
+
+class BitcoinUtilsTest {
 
 
     @Test
-    public void testScriptHashForLegacyP2PKH() {
+    void testScriptHashForLegacyP2PKH() {
         // examples taken from https://electrumx.readthedocs.io/en/latest/protocol-basics.html#script-hashes
-        String reversedScriptHash = BitcoinUtils.convertAddressToReversedScriptHash(Network.MAIN_NET, "1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa");
+        String reversedScriptHash = BitcoinUtils.convertAddressToReversedScriptHash(BitcoinNetwork.MAINNET, "1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa");
         assertEquals("8b01df4e368ea28f8dc0423bcf7a4923e3a12d307c875e47a0cfbf90b5c39161", reversedScriptHash);
     }
 }
